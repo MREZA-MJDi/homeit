77014162529fa3d545de73da08679d60106895ff
majidi1544 <mrezamajidi1998@gmail.com>
2026-07-09 23:50:41 +0330

    configure dashboard web routes

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::view('/dashboard', 'dashboard.index')->middleware('auth');
 
+Route::view('/dashboard', 'dashboard.index')->name('dashboard');
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
